730945979d14a0acbdd9f95be325f84451408350
smallville <abutimartin778@gmail.com>
2024-10-28 23:43:40 +0300

    c6

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,25 @@ import logo from '/logo.svg'
 import './App.css'
 
 function App() {
-  return (
-    <>
-      <div>
-        <a href="!#">
-          <img src={logo} className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <p>Smallville CDN</p>
-        <div className="card card-body">
-            <h6>SMV js V1.0.1</h6>
-            <p>https://cdn.bobgroganconsulting.com/js/smallville.min.js</p>
-        </div>
-    </>
-  )
+    const scriptTag = `<script src="https://cdn.bobgroganconsulting.com/js/smallville.min.js"></script>`;
+
+    return (
+        <>
+            <div>
+                <a href="!#">
+                    <img src={logo} className="logo" alt="Vite logo" />
+                </a>
+            </div>
+            <p>Smallville CDN</p>
+            <div className="carsd carsd-body">
+                <h6></h6>
+                <p>SMV js V1.0.1</p>
+                <textarea readOnly rows="3"  style={{ width: '50vw', outline:"none",border:"none",color:'#000',background:'rgba(0,0,0,0.06)',padding:10}} >
+                    {scriptTag}
+                </textarea>
+            </div>
+        </>
+    )
 }
 
-export default App
+export default App;
